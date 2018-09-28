b972aed98cbf099ab1ccf681aec3ea41fd3bc539
Dan at Work <danmendelowitz@gmail.com>
2018-09-28 12:51:06 +0200

    Added more assets

---
+++ Assets/Scripts/NavMeshClickToMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshClickToMove : MonoBehaviour
+{
+
+	public NavMeshAgent agent;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
