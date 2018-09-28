b1714a8796c8f32a5b034efa6ace7f6750ec4cf6
Dan at Work <danmendelowitz@gmail.com>
2018-09-28 17:27:24 +0200

    Added a lot of materials

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+	public string sceneName = "Box Sample";
+	public void Restart()
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
